422433f4877074c0970c373e59cbf9f381599266
Theodor Johansson <t.j.u@telia.com>
2023-12-01 13:00:47 +0100

    initial commit

---
+++ server/dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const Post_1 = require("./entities/Post");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [Post_1.Post],
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !constants_1.__prod__,
+    });
+};
+main();
+//# sourceMappingURL=index.js.map
